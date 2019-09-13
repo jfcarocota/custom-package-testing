752a42727bed64de717b630a3eae90004a64ee04
jfcarocota <jfcarocota@gmail.com>
2019-09-13 00:59:03 -0700

    publish package

---
+++ InputSystem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+namespace Core.InputSystem
+{
+    public class InputSystem 
+    {
+        ///<summary>
+        /// Returns the current axis direction.
+        ///</summary>
+        public static Vector2 Axis
+        {
+            get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+}
